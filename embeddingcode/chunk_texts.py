bc0f61c1e3aeafac053131f09db2ef3b66bf38bd
tedunderwood <tunder@illinois.edu>
2023-10-17 21:30:02 -0500

    limiting embeddings to found articles

--- embeddingcode/chunk_texts.py
+++ embeddingcode/chunk_texts.py
@@ -170,6 +170,7 @@ with open('../LitStudiesJSTOR.jsonl', encoding = 'utf-8') as f:
 		if 'identifier' in json_obj:
 			for idtype in json_obj['identifier']:
 				if idtype['name'] != 'local_doi':
+					print(idtype['value'])
 					alternateID = idtype['value'].split('/')[1]
 					if articleID != alternateID:
 						print('Discrepancy in IDs: url id', articleID, 'doi', alternateID)
