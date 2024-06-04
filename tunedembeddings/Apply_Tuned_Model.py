198ee1c7f8524b6b2f9e6abc15264921d0876651
tedunderwood <tunder@illinois.edu>
2024-06-03 23:04:38 -0500

    apply the model

---
+++ tunedembeddings/Apply_Tuned_Model.py
@@ -0,0 +1,63 @@
+# Apply Sentence-Bert Model
+
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+
+# 1. Load a pretrained Sentence Transformer model
+model = SentenceTransformer('models/final_20000pairs')
+
+meta = pd.read_csv("../metadata/litstudies/LitMetadataWithS2.tsv", sep = '\t')
+meta = meta[meta['paperId'].notnull() & (meta['paperId'] != '')]
+
+rootfolder = "../perplexity/cleanchunks/"
+
+paperIds = []
+chunknumbers = []
+paragraphs = []
+
+for decade in range(1900, 1950, 10):
+    print(decade)
+    this_decade = meta[(meta['year'] >= decade) & (meta['year'] < decade + 10)]
+    for idx, row in this_decade.iterrows():
+        paper_id = row['paperId']
+        path = rootfolder + paper_id + ".txt"
+        if not os.path.isfile(path):
+            continue
+        with open(path, 'r') as f:
+            textlines = f.readlines()
+            textlines = [line.strip().split('\t')[1] for line in textlines]
+        numberofchunks = len(textlines)
+        paperIds.extend([paper_id] * numberofchunks)
+        chunknumbers.extend(list(range(numberofchunks)))
+        paragraphs.extend(textlines)
+    
+    embeddings = model.encode(paragraphs, batch_size = 128, device = 'cuda')
+    numrows, numcols = embeddings.shape
+    # Create a dataframe with paperIds, chunknumbers, and embeddings
+    data = {'paperId': paperIds, 'chunknumber': chunknumbers}
+    for i in range(numcols):
+        data[f'embedding_{i+1}'] = embeddings[:, i]
+
+    df = pd.DataFrame(data)
+
+    outpath = "embeddings/" + str(decade) + ".tsv"
+    df.to_csv(outpath, sep = '\t', index = False)
+    
+
+
+
+
+
+        
+
+
+# The sentences to encode
+sentences = [
+    "The weather is lovely today.",
+    "It's so sunny outside!",
+    "He drove to the stadium.",
+]
+
+# 2. Calculate embeddings by calling model.encode()
+embeddings = model.encode(sentences)
+print(embeddings.shape)
